7aaf0fa9482401e86d345bd52ab0af205e3eb4d5
Heggyhere <heggyy@gmail.com>
2019-04-23 18:11:17 -0700

    changed index.js

--- models/index.js
+++ models/index.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/way-farer',
-  { useNewUrlParser: true })
-
+// mongoose.connect('mongodb://localhost/way-farer',
+//   { useNewUrlParser: true })
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/jwt-auth", {
+  useNewUrlParser: true
+});
 module.exports= {
     Post: require('./Post'),
     User: require('./User')
